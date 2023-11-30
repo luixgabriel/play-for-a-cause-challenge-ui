468dc1ddc8505e9d448ded866e41edf0a35ebd13
Luis Gabriel <luis16757@gmail.com>
2023-11-29 21:35:05 -0300

    feat: criando container que envovle a aplicação completa

--- src/app/components/container-main.tsx
+++ src/app/components/container-main.tsx
@@ -1,11 +1,10 @@
 interface ContainerMainProps {
-    children: React.ReactNode
-  }
-  
+  children: React.ReactNode
+}
 
 const ContainerMain = ({ children }: ContainerMainProps) => {
   return (
-    <main className="p-5 w-screen h-screen bg-gradient-to-r from-blue-300 to-teal-500">
+    <main className="w-screen h-screen bg-gradient-to-r from-blue-300 to-teal-500 flex items-center justify-center">
       {children}
     </main>
   )
