cd36483e98773ce590d951beae65a960dbbcc94e
Luis Gabriel <luis16757@gmail.com>
2023-11-29 21:34:14 -0300

    chore: config inicial de estilos

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,9 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      backgroundColor: {
+        primary: '#99F6E4',
+      },
     },
   },
   plugins: [],
