2d3de76669d97174da9ca7c94be4dbd69063ff84
Luis Gabriel <luis16757@gmail.com>
2023-11-30 09:41:51 -0300

    chore: adicionando dados estáticos para teste

---
+++ src/data/messages.ts
@@ -0,0 +1,78 @@
+export interface IMessage {
+  id: string
+  senderId: string
+  content: string
+  createdAt: string
+}
+
+const messages: IMessage[] = [
+  {
+    id: '1',
+    senderId: '1',
+    content: 'Opa, boa noite!',
+    createdAt: '25/08/2053',
+  },
+
+  {
+    id: '2',
+    senderId: '2',
+    content: 'Opa, boa noite!',
+    createdAt: '25/08/2053',
+  },
+  {
+    id: '3',
+    senderId: '1',
+    content: 'Opa, boa noite!',
+    createdAt: '25/08/2053',
+  },
+  {
+    id: '4',
+    senderId: '1',
+    content: 'Opa, boa noite!',
+    createdAt: '25/08/2053',
+  },
+  {
+    id: '5',
+    senderId: '2',
+    content: 'O Vasco não cai!',
+    createdAt: '25/08/2053',
+  },
+  {
+    id: '5',
+    senderId: '2',
+    content: 'O Vasco não cai!',
+    createdAt: '25/08/2053',
+  },
+  {
+    id: '5',
+    senderId: '1',
+    content: 'O Vasco não cai!',
+    createdAt: '25/08/2053',
+  },
+  {
+    id: '5',
+    senderId: '2',
+    content: 'O Vasco não cai!',
+    createdAt: '25/08/2053',
+  },
+  {
+    id: '5',
+    senderId: '2',
+    content: 'O Vasco não cai!',
+    createdAt: '25/08/2053',
+  },
+  {
+    id: '5',
+    senderId: '1',
+    content: 'O Vasco não cai!',
+    createdAt: '25/08/2053',
+  },
+  {
+    id: '5',
+    senderId: '2',
+    content: 'O Vasco não cai!',
+    createdAt: '25/08/2053',
+  },
+]
+
+export default messages
---
+++ src/data/users.ts
@@ -0,0 +1,60 @@
+export interface IUser {
+  id: string
+  name: string
+  imageUrl: string
+}
+
+const users: IUser[] = [
+  {
+    id: '1',
+    name: 'Usuário 1',
+    imageUrl: 'https://avatars.githubusercontent.com/u/70019908?v=4',
+  },
+  {
+    id: '2',
+    name: 'Usuário 2',
+    imageUrl: 'https://avatars.githubusercontent.com/u/70019908?v=4',
+  },
+  {
+    id: '3',
+    name: 'Usuário 3',
+    imageUrl: 'https://avatars.githubusercontent.com/u/70019908?v=4',
+  },
+  {
+    id: '4',
+    name: 'Usuário 4',
+    imageUrl: 'https://avatars.githubusercontent.com/u/70019908?v=4',
+  },
+  {
+    id: '5',
+    name: 'Usuário 5',
+    imageUrl: 'https://avatars.githubusercontent.com/u/70019908?v=4',
+  },
+  {
+    id: '6',
+    name: 'Usuário 6',
+    imageUrl: 'https://avatars.githubusercontent.com/u/70019908?v=4',
+  },
+  {
+    id: '7',
+    name: 'Usuário 7',
+    imageUrl: 'https://avatars.githubusercontent.com/u/70019908?v=4',
+  },
+  {
+    id: '8',
+    name: 'Usuário 8',
+    imageUrl: 'https://avatars.githubusercontent.com/u/70019908?v=4',
+  },
+  {
+    id: '9',
+    name: 'Usuário 9',
+    imageUrl: 'https://avatars.githubusercontent.com/u/70019908?v=4',
+  },
+  {
+    id: '10',
+    name: 'Usuário 10',
+    imageUrl: 'https://avatars.githubusercontent.com/u/70019908?v=4',
+  },
+]
+
+export default users
