4ae7fd73141057defd02f78c2e2322213465b659
Luis Gabriel <luis16757@gmail.com>
2023-12-03 15:55:34 -0300

    fix: adicionando a url de prod

--- src/app/lib/axios.ts
+++ src/app/lib/axios.ts
@@ -1,5 +1,5 @@
 import axios from 'axios'
 // https://play-for-a-cause-chat.onrender.com
 export default axios.create({
-  baseURL: 'http://localhost:3001',
+  baseURL: 'https://play-for-a-cause-chat.onrender.com',
 })
