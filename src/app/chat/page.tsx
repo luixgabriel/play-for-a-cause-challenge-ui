f149f9e20259d33fb2d0f2e3f73cd46504044825
Luis Gabriel <luis16757@gmail.com>
2023-12-03 09:03:37 -0300

    feat: adicionando lógica de chat, user online e chats do usuário

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -1,6 +1,5 @@
 'use client'
 import Image from 'next/image'
-import messages from '../../data/messages'
 import ChatBox from '../components/chatbox'
 import ContainerMain from '../components/container-main'
 import { useChat } from '../hooks/useChat'
@@ -9,12 +8,38 @@ import ProfilePicture from '../components/profile-picture'
 import { IOnlineUsers } from '../../types/online-users'
 import { useRouter } from 'next/navigation'
 import Cookies from 'js-cookie'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
+import { UsersRound, MessageSquare } from 'lucide-react'
+import { IUserChats } from '../../types/user-chats'
+import { useCreateChatMutate } from '../hooks/useCreateChatMutate'
+import { toast } from 'react-toastify'
+import { useAuth } from '../hooks/useAuth'
 
 const ContainerChat = () => {
-  const { onlineUsers } = useChat()
+  const [receiver, setReceiver] = useState<IUserChats | null>(null)
+  const { mutate } = useCreateChatMutate()
+  const { onlineUsers, userChats, isLoading } = useChat()
+  const { user } = useAuth()
   const router = useRouter()
   const token = Cookies.get('token')
+  let chatIsExists: boolean
+
+  const createChat = async (receiverId: string) => {
+    userChats?.forEach((item) => {
+      if (item.participants[0].id === receiverId) {
+        chatIsExists = true
+      }
+    })
+
+    if (chatIsExists) {
+      toast.info('Você já tem um chat com esse usuário.')
+      return
+    }
+
+    if (user) {
+      mutate({ userId: user.id, receiverId })
+    }
+  }
 
   useEffect(() => {
     if (!token) router.push('/')
@@ -26,9 +51,16 @@ const ContainerChat = () => {
         <div className="flex h-full text-slate-950 gap-5">
           <div className="bg-slate-500 bg-opacity-40 h-full w-[32%] rounded-lg shadow-lg">
             <Profile />
+            <span className="p-1 ml-4 flex gap-2 text-gray-700">
+              <UsersRound />
+              Usuários online:
+            </span>
             <div className="p-2 flex flex-col">
               {onlineUsers?.map((item: IOnlineUsers) => (
-                <div key={item.user.id} className="flex items-center gap-2 p-2">
+                <div
+                  key={item.user.id}
+                  className="flex items-center gap-2 p-2 cursor-pointer hover:bg-slate-200 hover:bg-opacity-30 rounded-lg"
+                >
                   <div className="flex items-center gap-2">
                     <div className="relative">
                       {item?.user.imageUrl ? (
@@ -52,13 +84,62 @@ const ContainerChat = () => {
                   </div>
                   <div className="flex flex-col">
                     <span className="text-sm">{item.user.name}</span>
-                    <span>Comece uma conversa!</span>
+                    <button onClick={() => createChat(item.user.id)}>
+                      Iniciar chat
+                    </button>
                   </div>
                 </div>
               ))}
             </div>
+            <span className="p-1 ml-4 flex gap-2 text-gray-700">
+              <MessageSquare />
+              Seus chats:
+            </span>
+            {isLoading ? (
+              <h1>carregando</h1>
+            ) : userChats?.length === 0 ? (
+              <h1>sem chats</h1>
+            ) : (
+              userChats?.map((item: IUserChats) => (
+                <div
+                  key={item.id}
+                  className="px-2 py-1"
+                  onClick={() => setReceiver(item)}
+                >
+                  <div className="flex items-center gap-2 p-2 cursor-pointer hover:bg-slate-200 hover:bg-opacity-30 rounded-lg">
+                    <div className="flex items-center gap-2">
+                      <div className="relative">
+                        {item?.participants[0].imageUrl ? (
+                          <>
+                            <div className="w-12 h-12 overflow-y-hidden rounded-full flex items-center bg-red-500 justify-center">
+                              <Image
+                                src={item.participants[0].imageUrl}
+                                width={100}
+                                height={100}
+                                alt="user-icon"
+                                className="rounded-full cursor-pointer"
+                              />
+                            </div>
+                          </>
+                        ) : (
+                          <ProfilePicture widht="48" height="48" />
+                        )}
+
+                        <div className="absolute bottom-0 right-0 h-3 w-3 bg-green-500 rounded-full"></div>
+                      </div>
+                    </div>
+                    <div className="flex flex-col">
+                      <span className="text-sm">
+                        {item.participants[0].name}
+                      </span>
+                      <span>{item.lastMessage}</span>
+                    </div>
+                  </div>
+                </div>
+              ))
+            )}
           </div>
-          <ChatBox messages={messages} />
+          <ChatBox receiver={receiver} />
         </div>
       </div>
     </ContainerMain>
--- src/app/components/chatbox.tsx
+++ src/app/components/chatbox.tsx
@@ -1,32 +1,60 @@
-import { MessagesSquare, SendHorizonal } from 'lucide-react'
+import { Laugh, MessagesSquare, SendHorizonal } from 'lucide-react'
 import { IMessage } from '../../data/messages'
+import { useEffect, useState } from 'react'
+import { useAuth } from '../hooks/useAuth'
+
+import { io } from 'socket.io-client'
+import { IUserChats } from '../../types/user-chats'
 
 interface IChatBoxProps {
-  messages: IMessage[]
+  messages?: IMessage[]
+  receiver: IUserChats | null
 }
 
-const ChatBox = ({ messages }: IChatBoxProps) => {
-  const userId = '1'
+const ChatBox = ({ receiver }: IChatBoxProps) => {
+  const [message, setMessage] = useState<string>('')
+  const [messageList, setMessageList] = useState<any>([])
+  const { user } = useAuth()
+
+  // useEffect(() => {
+  //   newSocket.on('sendMessage', (data) => {
+  //     console.log(data)
+  //     setMessageList((prev) => [...prev, message])
+  //   })
+  // }, [messageList])
+
+  // const enviaMensagem = async () => {
+  //   newSocket.emit('sendMessage', message)
+  // }
   return (
     <div className="w-[68%] flex flex-col place-content-between bg-slate-500 bg-opacity-40 shadow-lg rounded-lg">
       <div className="bg-neutral-800 bg-opacity-10 w-full flex justify-center items-center p-5 gap-4 overflow-y-hidden">
-        <MessagesSquare width={55} height={55} />
-        <span className="text-2xl">Luis Gabriel</span>
+        <MessagesSquare width={48} height={48} />
+        <span className="text-2xl">{receiver?.participants[0].name || ''}</span>
       </div>
       <div className="h-full">
         <div className="flex flex-col p-3">
-          {messages.map((item) => (
-            <span
-              key={item.id}
-              className={`p-3  rounded-2xl justify-center my-2 mx-1 ${
-                item.senderId === userId
-                  ? 'self-start bg-blue-400'
-                  : 'self-end bg-blue-200 '
-              }`}
-            >
-              {item.content}
-            </span>
-          ))}
+          {receiver ? (
+            receiver.Messages?.map((item: IMessage) => (
+              <span
+                key={item.id}
+                className={`p-3  rounded-2xl justify-center my-2 mx-1 ${
+                  item.senderId === user?.id
+                    ? 'self-start bg-blue-400'
+                    : 'self-end bg-blue-200 '
+                }`}
+              >
+                {item.content}
+              </span>
+            ))
+          ) : (
+            <div className="flex items-center justify-center mt-52">
+              <h1 className="text-2xl flex items-center gap-2">
+                Tome inciativa! Inicie uma conversa
+                <Laugh size={30} />
+              </h1>
+            </div>
+          )}
         </div>
       </div>
       <div className="flex justify-center items-center my-3 gap-2 overflow-y-hidden h-16 ">
